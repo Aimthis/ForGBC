5315941e46a8cead564aff590744c7db69a46336
aimthis <aimthis@mail.ru>
2023-06-21 03:20:14 +0500

    end 21062023 dz27

--- Seminars/21062023/DZ29/Program.cs
+++ Seminars/21062023/DZ29/Program.cs
@@ -2,8 +2,9 @@
 
 //Создаю и наполняю массив случайными числами от 0 до 2 147 483 647
 int[] array = new int[8];
+Console.Write("Создан массив { | ");
 for(short i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0,2147483648);
-        Console.Write("Создан массив {" + array[i] + " , ");
+        array[i] = new Random().Next(0,2147483647);
+        Console.Write(array[i] + " | ");
     }   Console.Write("}");
