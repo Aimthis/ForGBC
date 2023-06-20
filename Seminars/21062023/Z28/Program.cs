9eeed97860e96430383a14264078b1bb4e06c13f
aimthis <aimthis@mail.ru>
2023-06-21 01:31:54 +0500

    end 21062023 z28

--- Seminars/21062023/Z28/Program.cs
+++ Seminars/21062023/Z28/Program.cs
@@ -2,5 +2,18 @@
 4 -> 24 
 5 -> 120*/
 
-
-
+// Создаю переменную Ui - User Input с запросом на ввод
+    Console.Write("Пожалуйста, введите число: ");
+    int Ui = int.Parse(Console.ReadLine());
+// Задаю метод факториала
+    double GetFactorial(int a)
+    {
+        int result = 1;
+        for(int i = 1; i < a; i++)
+        {
+            result *= i;
+        }
+    }
+// Вывод в консоль
+    Console.Write(GetFactorial(Ui));
+    
