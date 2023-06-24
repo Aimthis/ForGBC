1e3e22f2d14c6998301bbe55329348c6ababbc26
aimthis <aimthis@mail.ru>
2023-06-24 14:14:53 +0500

    start SimpleCalc

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача: Создать простой калькулятор на 4 действия - сложения, вычетания, произведения и деления.
+
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("-=Простой калькулятор=-");
+         = Console.ReadKey().Key;       
+    }
+}
+
