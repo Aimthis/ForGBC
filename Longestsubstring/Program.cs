873cf9390e37fd7f3962c2c63e79669591b796bd
aimthis <aimthis@mail.ru>
2023-06-27 23:57:50 +0500

    seminar 27062023 Z42; Z44; Z45

--- Longestsubstring/Program.cs
+++ Longestsubstring/Program.cs
@@ -1,14 +1,22 @@
 ﻿public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        string out = s[0];
-        string check = s[s.length - 1];
-        while(out.length == check.length){
-            for(int i = 1; i + 1 < s.length; i++){
-                if(out != s[out.length])
-                out += s[i];
-                for(int j = s.length - 1; j > 0; j--){
-                  if(check[j])
+        string substring = s[0];
+        string LongestSubstring = string.Empty;
+        for(int end = 0; end < s.Length; end ++){
+            for(int i = 0; i < s.Length; i++){
+                if(substring[i] != s[substring.Length])
+                    substring += s[substring.Length];
+            }
+            if(substring.Length < s.Length){
+                string check = substring[substring.Length - 1];
+                for(int j = substring.Length + 1; j < s.Length; j++){
+                    if(check[j - 1] != s[j])
+                        check += s[j];
                 }
+            if(substring.Length > check.Length)
+                LongestSubstring = substring;
+            else
+                LongestSubstring = check;
             }
         }        
     }
