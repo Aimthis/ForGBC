8e7465e9769bec0649df0d4d2848f84ee2b1149d
aimthis <aimthis@mail.ru>
2023-06-30 00:52:29 +0500

    done dz43

--- Seminars/27062023/DZ43/Program.cs
+++ Seminars/27062023/DZ43/Program.cs
@@ -2,36 +2,41 @@
  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
 
 // Создаю строковые переменные Aui - all user input для сбора данных и Ui - user input для ввода
-string Aui = string.Empty;
+double[] Aui = new double[4];
 string Ui = string.Empty;
 
 // Создаю запрос на ввод в цикле до ввода 4 значений
 Console.Clear();
 Console.WriteLine("Введите числа 1 = b1; 2 = k1; 3 = b2; 4 = k2");
 Console.WriteLine();
-while(Aui.Length < 4){
-    Console.Write("Введите " + (Aui.Length + 1) + " число : ");
+for(int i = 0;i < Aui.Length; i++){
+    Console.Write("Введите " + (i + 1) + " число : ");
     Ui = Console.ReadLine();
     Console.WriteLine();
     try{
-        double.Parse(Ui);    
-        Aui += Ui;
+        double convert = Convert.ToDouble(Ui);    
+        Aui[i] = convert;
     }
     catch (System.Exception){
         Console.WriteLine(" ");
         Console.WriteLine("Ошибка в переводе строки в число. Пожалуйста введите число");
+        i--;
         continue;
+    }
+}
+// Проверка на пересечение прямых
+if(Aui[1] == Aui[3]){
+    if(Aui[0] == Aui[2]){
+        Console.Write("Прямые совпадают");
+        Environment.Exit(0);
+    }
+    else{
+        Console.Write("Прямые параллельны и не имеют точек пересечения");
+        Environment.Exit(0);
     }
 }
 
 Console.WriteLine($"Точка пересечения двух прямых : y = {Aui[1]} * x + {Aui[0]}, y = {Aui[3]} * x + {Aui[2]}");
-/*
-int x = 0;
-int y = 0;
-while(Aui[1] * x + Aui[0] != y){
-    x++;
-    while(Aui[3] * x + Aui[2] = y){
-        y++;
-    }
-}*/
-Console.Write($"Имеет координаты : ({0}, {0})");
+double x = (Aui[2] - Aui[0]) / (Aui[1] - Aui[3]);
+double y = Aui[1] * x + Aui[0];
+Console.Write($"Имеет координаты : ({x}, {y})");
