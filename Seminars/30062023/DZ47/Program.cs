fbf9ce48dcffdd1eade605a588b26eaf2adb0ad5
aimthis <aimthis@mail.ru>
2023-07-03 04:25:21 +0500

    Start DZ47 for seminar 30062023

---
+++ Seminars/30062023/DZ47/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+*/
+// Создаю переменные для строк и колонок - rows и colums соответственно с запросом на ввод
+Console.Clear();
+Console.Write("Пожалуйста, введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите количество колонок: ");
+int colums = int.Parse(Console.ReadLine());
+Console.WriteLine("Создан двумерный массив содержащий следующие значения:");
+double[,] matrix = new double[rows,colums];
+for(int i = 0; i < rows; i++){
+    Console.WriteLine();
+    for(int j = 0; j < colums; j++){
+        matrix[i,j] = new Random().NextDouble(-1,2);
+        Console.Write(" " + matrix[i,j]);
+    }
+}
