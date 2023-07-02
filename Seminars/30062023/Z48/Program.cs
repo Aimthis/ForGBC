bde0a41c163c0a638a416a581036eec6451c30d9
aimthis <aimthis@mail.ru>
2023-07-03 02:44:29 +0500

    start Z49 for seminar 30062023

--- Seminars/30062023/Z48/Program.cs
+++ Seminars/30062023/Z48/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+// Создаю переменные для строк и колонок - rows и colums соответственно с запросом на ввод
+Console.Clear();
+Console.Write("Пожалуйста, введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите количество колонок: ");
+int colums = int.Parse(Console.ReadLine());
+Console.WriteLine("Создан двумерный массив содержащий следующие значения:");
+int[,] matrix = new int[rows,colums];
+for(int i = 0; i < rows; i++){
+    Console.WriteLine();
+    for(int j = 0; j < colums; j++){
+        matrix[i,j] = i + j;
+        Console.Write(" " + matrix[i,j]);
+    }
+}
