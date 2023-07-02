5ee51bf887f44310c8b8d4b53a0d1ec753681f4f
aimthis <aimthis@mail.ru>
2023-07-03 04:57:12 +0500

    start DZ50 for seminar 30062023

---
+++ Seminars/30062023/Z50/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+// Создаем двумерный массив на 8-16 чисел и случайными значениями внутри от 0 до 999  
+double[,] array = new double[new Random().Next(8,17),new Random().Next(8,17)];
+for(int i = 0; i < array.GetLength(0);i++){
+    for(int j = 0; j < array.GetLength(1);j++){
+        array[i,j] = new Random().Next(0,1000);
+    }
+}
+// Создаю переменные для поиска строки и столбца в массиве с запросом на ввод
+Console.Clear();
+Console.Write("Пожалуйста, введите строку: ");
+int searchString = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+Console.Write("Пожалуйста, введите столбец: ");
+int searchColumn = int.Parse(Console.ReadLine());
+
+try
+{
+    Console.WriteLine($"Элемент в строке {searchString} и столбце {searchColumn} имеет значение : " + array[searchString,searchColumn]);
+}
+catch (System.Exception)
+{
+    Console.WriteLine("В массиве нет такого элемента");
+    Environment.Exit;
+}
+
+/*
+for(int i = 0;i < array.GetLength(0);i++){
+    for(int j = 0;j < array.GetLength(1);j++){
+        if(Ui == array[i,j])
+            Console.WriteLine("В массиве есть такое число");
+        else
+            Console.Write("В массиве нет такого числа");
+    }
+}*/
