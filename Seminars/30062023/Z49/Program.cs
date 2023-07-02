a34cab7a9c33a8aa64952a36e5777eb496975bdc
aimthis <aimthis@mail.ru>
2023-07-03 03:14:08 +0500

    end Z51 for seminar 30062023

--- Seminars/30062023/Z49/Program.cs
+++ Seminars/30062023/Z49/Program.cs
@@ -8,6 +8,7 @@ int rows = int.Parse(Console.ReadLine());
 Console.Write("Пожалуйста, введите количество колонок: ");
 int colums = int.Parse(Console.ReadLine());
 Console.WriteLine("Создан двумерный массив содержащий следующие значения:");
+// Создаю двумерный массив по полученным значениям строк и колонок
 int[,] matrix = new int[rows,colums];
 for(int i = 0; i < rows; i++){
     Console.WriteLine();
@@ -19,6 +20,7 @@ for(int i = 0; i < rows; i++){
 Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine("Квадраты элементов массива с четными индексами");
+// Ищу элементы с четными строками и столбцами соответственно с четными индексами и меняю их значения на их квадраты
 for(int i = 1; i < rows; i += 2){
     for(int j = 1; j < colums; j+= 2){
         matrix[i,j] *= matrix[i,j];
@@ -28,6 +30,7 @@ for(int i = 1; i < rows; i += 2){
 Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine("Массив после замены");
+// Вывод полученного массива в консоль
 for(int i = 0; i < rows; i++){
     Console.WriteLine();
     for(int j = 0; j < colums; j++){
