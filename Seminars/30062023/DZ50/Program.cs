e005a1c5b840c512d46fdbe65c3889d0c7c2a901
aimthis <aimthis@mail.ru>
2023-07-03 05:17:57 +0500

    start DZ52 for seminar 30062023

--- Seminars/30062023/DZ50/Program.cs
+++ Seminars/30062023/DZ50/Program.cs
@@ -2,10 +2,10 @@
 и возвращает значение этого элемента или же указание, что такого элемента нет.*/
 
 // Создаем двумерный массив на 8-16 строк, столбцов и случайными значениями внутри от 0 до 999  
-double[,] array = new double[new Random().Next(8,17),new Random().Next(8,17)];
-for(int i = 0; i < array.GetLength(0);i++){
-    for(int j = 0; j < array.GetLength(1);j++){
-        array[i,j] = new Random().Next(0,1000);
+double[,] matrix = new double[new Random().Next(8,17),new Random().Next(8,17)];
+for(int i = 0; i < matrix.GetLength(0);i++){
+    for(int j = 0; j < matrix.GetLength(1);j++){
+        matrix[i,j] = new Random().Next(0,1000);
     }
 }
 // Создаю переменные для поиска строки и столбца в массиве с запросом на ввод
@@ -18,7 +18,7 @@ int searchColumn = int.Parse(Console.ReadLine());
 
 try
 {
-    Console.WriteLine($"Элемент в строке {searchString} и столбце {searchColumn} имеет значение : " + array[searchString,searchColumn]);
+    Console.WriteLine($"Элемент в строке {searchString} и столбце {searchColumn} имеет значение : " + matrix[searchString,searchColumn]);
 }
 catch (System.Exception)
 {
---
+++ Seminars/30062023/DZ52/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+*/
+
+// Создаем двумерный массив на 8-16 строк, столбцов и случайными значениями внутри от 0 до 999  
+Console.Clear();
+Console.WriteLine("Создан двумерный массив содержащий следующие значения:");
+Console.WriteLine(" ");
+double[,] matrix = new double[new Random().Next(8,17),new Random().Next(8,17)];
+// Создаю переменные для подсчета результата.
+string itog = string.Empty;
+double SredneePoStolbcu = 0;
+for(int i = 0; i < matrix.GetLength(1);i++){
+    for(int j = 0; j < matrix.GetLength(0);j++){
+        matrix[i,j] = new Random().Next(0,1000);
+        SredneePoStolbcu+= matrix[i,j];
+        Console.Write(" " + matrix[i,j]);
+    }
+    SredneePoStolbcu/=matrix.GetLength(0);
+    itog += SredneePoStolbcu;
+}
+Console.WriteLine(" ");
+Console.WriteLine($"Массив содержит {matrix.GetLength(1)} столбцов");
+Console.WriteLine("Среднее арифметическое каждого столбца :" + itog);
