55ea9743a0957310451603cd3728d6eaa94d6983
aimthis <aimthis@mail.ru>
2023-07-03 06:17:50 +0500

    end DZ52 for seminar 30062023

--- Seminars/30062023/DZ52/Program.cs
+++ Seminars/30062023/DZ52/Program.cs
@@ -6,18 +6,27 @@ Console.Clear();
 Console.WriteLine("Создан двумерный массив содержащий следующие значения:");
 Console.WriteLine(" ");
 double[,] matrix = new double[new Random().Next(8,17),new Random().Next(8,17)];
-// Создаю переменные для подсчета результата.
-string itog = string.Empty;
-double SredneePoStolbcu = 0;
-for(int i = 0; i < matrix.GetLength(1);i++){
-    for(int j = 0; j < matrix.GetLength(0);j++){
+for(int i = 0; i < matrix.GetLength(0);i++){
+    for(int j = 0; j < matrix.GetLength(1);j++){
         matrix[i,j] = new Random().Next(0,1000);
-        SredneePoStolbcu+= matrix[i,j];
         Console.Write(" " + matrix[i,j]);
     }
-    SredneePoStolbcu/=matrix.GetLength(0);
-    itog += SredneePoStolbcu;
+    Console.WriteLine(" ");
 }
 Console.WriteLine(" ");
 Console.WriteLine($"Массив содержит {matrix.GetLength(1)} столбцов");
-Console.WriteLine("Среднее арифметическое каждого столбца :" + itog);
+// Создаю переменные для подсчета результата.
+string itog = "|";
+double SredneePoStolbcu = 0;
+// Цикл для подсчета среднеарифметического каждого столбца
+for(int i = 0;i<matrix.GetLength(1);i++){
+    for(int j=0;j<matrix.GetLength(0);j++){
+        SredneePoStolbcu += matrix[j,i];
+    }
+    SredneePoStolbcu /= matrix.GetLength(1);
+    SredneePoStolbcu = Math.Round(SredneePoStolbcu , 1);
+    itog += $"| {i+1}. {SredneePoStolbcu} |";
+    SredneePoStolbcu = 0;
+}
+itog += "|";
+Console.WriteLine("Среднее арифметическое каждого столбца : " + itog);
