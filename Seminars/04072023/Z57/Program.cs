9ed2c2b43630ddb3921468909581b5750db16586
aimthis <aimthis@mail.ru>
2023-07-05 01:34:37 +0500

    end z57 seminar 07042023

--- Seminars/04072023/Z57/Program.cs
+++ Seminars/04072023/Z57/Program.cs
@@ -14,4 +14,19 @@ for(int i = 0; i < matrix.GetLength(0);i++){
     }
     Console.WriteLine(" ");
 }
+int Ui;
+do{
 Console.WriteLine(" ");
+// Создаю переменную Ui - User Input с запросом на ввод и счетчик
+    Console.Write("Пожалуйста, введите число(10 для выхода): ");
+    Ui = Convert.ToInt32(Console.ReadLine());
+    int count = 0;
+for(int i = 0; i < matrix.GetLength(0);i++){
+    for(int j = 0; j < matrix.GetLength(1);j++){
+        if(Ui == matrix[i,j])
+            count++;
+    }
+}
+Console.WriteLine("Данное число встречается " + count + " раз");
+}
+while(Ui != 10);
