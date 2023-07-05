c20256751ef506048b8b66478c39f48a9efc3bb4
aimthis <aimthis@mail.ru>
2023-07-06 03:31:15 +0500

    point 3:31 Z61 seminar 04072023

--- Seminars/04072023/Z61/Program.cs
+++ Seminars/04072023/Z61/Program.cs
@@ -2,21 +2,21 @@
 // Создаю переменную для N с запросом на ввод
 Console.Clear();
 int rowsN = StringToInt("Пожалуйста, введите количество строк треугольника Паскаля: ");
-double[,] pascalData = new double[rowsN,rowsN*2+1];
-pascalData[0,rowsN] = 1;
-Console.WriteLine(pascalData[0,rowsN] + " ");
-pascalData[1,rowsN] = 1;
-pascalData[1,rowsN+1] = 1;
-Console.WriteLine(pascalData[1,rowsN] + " " + pascalData[1,rowsN+1]);
+double[,] pascalData = new double[rowsN,rowsN+1];
+pascalData[0,1] = 1;
+pascalData[1,1] = 1;
+pascalData[1,2] = 1;
 for(int i=2;i<pascalData.GetLength(0);i++){
-    pascalData[i,rowsN-i] = 1;
-    for(int j=rowsN; j<pascalData.GetLength(1); j++){
+    for(int j=1; j<pascalData.GetLength(1); j++){
     pascalData[i,j]+=pascalData[i-1,j]+pascalData[i-1,j-1];
+    }
+}
+for(int i=0;i<pascalData.GetLength(0);i++){
+    for(int j=0; j<pascalData.GetLength(1); j++){
     Console.Write(pascalData[i,j] + " ");
     }
     Console.WriteLine();
 }
-
 
 
 
