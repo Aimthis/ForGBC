e08282af78ae0295db51ef725e5ca5099b1a8b94
aimthis <aimthis@mail.ru>
2023-07-06 00:37:24 +0500

    remake dz47 seminar 30062023

--- Seminars/30062023/DZ47/Program.cs
+++ Seminars/30062023/DZ47/Program.cs
@@ -1,17 +1,26 @@
-﻿/*Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿﻿/*Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 */
 // Создаю переменные для строк и колонок - rows и colums соответственно с запросом на ввод
 Console.Clear();
-Console.Write("Пожалуйста, введите количество строк: ");
-int rows = int.Parse(Console.ReadLine());
-Console.Write("Пожалуйста, введите количество колонок: ");
-int colums = int.Parse(Console.ReadLine());
-Console.WriteLine("Создан двумерный массив содержащий следующие значения:");
-double[,] matrix = new double[rows,colums];
-for(int i = 0; i < rows; i++){
-    Console.WriteLine();
-    for(int j = 0; j < colums; j++){
-        matrix[i,j] = new Random().NextDouble();
-        Console.Write(" " + matrix[i,j]);
+int rows = StringToInt("Пожалуйста, введите количество строк: ");
+int colums = StringToInt("Пожалуйста, введите количество столбцов: ");
+Array2d(rows,colums);
+
+int StringToInt(string message){
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+double[,] Array2d(int columnsLenght, int rowLenght){
+    Console.WriteLine("Создан двумерный массив содержащий следующие значения:");
+    double[,] matrix = new double[rows,colums];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < colums; j++){
+            matrix[i,j] = new Random().NextDouble();
+            Console.Write(" " + matrix[i,j]);
+        }
+        Console.WriteLine();
     }
+    return matrix;
 }
