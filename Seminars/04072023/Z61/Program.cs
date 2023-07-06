a76d9e43074edb3d057ef9e90f1041625eaaf060
aimthis <aimthis@mail.ru>
2023-07-06 11:55:36 +0500

    end Z61 seminar 04072023

--- Seminars/04072023/Z61/Program.cs
+++ Seminars/04072023/Z61/Program.cs
@@ -2,29 +2,39 @@
 // Создаю переменную для N с запросом на ввод
 Console.Clear();
 int rowsN = StringToInt("Пожалуйста, введите количество строк треугольника Паскаля: ");
-double[,] pascalData = new double[rowsN,rowsN+1];
-pascalData[0,1] = 1;
-pascalData[1,1] = 1;
-pascalData[1,2] = 1;
-for(int i=2;i<pascalData.GetLength(0);i++){
-    for(int j=1; j<pascalData.GetLength(1); j++){
-    pascalData[i,j]+=pascalData[i-1,j]+pascalData[i-1,j-1];
+double[,] pascalData = new double[rowsN, rowsN + 1];
+pascalData[0, 1] = 1;
+pascalData[1, 1] = 1;
+pascalData[1, 2] = 1;
+for (int i = 2; i < pascalData.GetLength(0); i++)
+{
+    for (int j = 1; j < pascalData.GetLength(1); j++)
+    {
+        pascalData[i, j] += pascalData[i - 1, j] + pascalData[i - 1, j - 1];
     }
 }
-for(int i=0;i<pascalData.GetLength(0);i++){
-    for(int j=0; j<pascalData.GetLength(1); j++){
-    Console.Write(pascalData[i,j] + " ");
+for (int i = 0; i < pascalData.GetLength(0); i++)
+{
+    FillConsoleWithSpace(rowsN - i);
+    for (int j = 0; j < pascalData.GetLength(1); j++)
+    {
+        if (pascalData[i, j] != 0)
+            Console.Write(pascalData[i, j] + " ");
     }
     Console.WriteLine();
 }
 
+void FillConsoleWithSpace(int count)
+{
+    while (count > 0)
+    {
+        Console.Write(" ");
+        count--;
+    }
+}
 
-
-
-
-
-
-int StringToInt(string message){
+int StringToInt(string message)
+{
     Console.Write(message);
     string value = Console.ReadLine();
     int result = Convert.ToInt32(value);
