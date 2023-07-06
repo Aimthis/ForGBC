1c8791c888fbfe91bfef5751a7a50547cac2a141
aimthis <aimthis@mail.ru>
2023-07-06 15:01:30 +0500

    point 15:01  DZ62 seminar 04072023

--- Seminars/04072023/DZ62/Program.cs
+++ Seminars/04072023/DZ62/Program.cs
@@ -1,6 +1,14 @@
 ﻿/*Задача 62: Заполните спирально массив 4 на 4.*/
 
+double[,] matrix = Array2dSpiralFill();
 
+// Вывод полученного массива в консоль
+for(int i = 0; i < matrix.GetLength(0); i++){
+    Console.WriteLine();
+    for(int j = 0; j < matrix.GetLength(1); j++){
+        Console.Write(" " + matrix[i,j]);
+    }
+}
 
 double[,] Array2dSpiralFill()
 {
@@ -9,10 +17,11 @@ double[,] Array2dSpiralFill()
     double[,] matrix = new double[new Random().Next(4, 5), new Random().Next(4, 5)];
     int fillValue = 1;
     int size = matrix.GetLength(0);
-    int row =0;
-    while(size != 0)
+    int row = 0;
+    int column = 0;
+    while (size != 0)
     {
-        for (int i = row; i < size; i++)
+        for (int i = column; i < size; i++)
         {
             matrix[row, i] = fillValue;
             fillValue++;
@@ -24,16 +33,21 @@ double[,] Array2dSpiralFill()
             matrix[j, size] = fillValue;
             fillValue++;
         }
-        row=size;
-        for(int k = size-1; k>row-size;k--){
-            matrix[row,k] = fillValue;
+        row = size;
+        for (int k = size - 1; k > row - size; k--)
+        {
+            matrix[row, k] = fillValue;
             fillValue++;
         }
         row--;
         size--;
-        for(int l = row;;l-- ){
-
+        for (int l = row; l > column+1; l--)
+        {
+            matrix[l, column] = fillValue;
+            fillValue++;
         }
+        column++;
+        row=column;
     }
     return matrix;
 }
