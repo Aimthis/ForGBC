87b04b618a4aea103ed8a1ad0c85f7a27cd46229
aimthis <aimthis@mail.ru>
2023-07-17 20:50:40 +0500

    end DZ64 seminar 15072023

--- Seminars/15072023/DZ64/Program.cs
+++ Seminars/15072023/DZ64/Program.cs
@@ -2,3 +2,24 @@
 M = 1; N = 5. -> "1, 2, 3, 4, 5"
 M = 4; N = 8. -> "4, 6, 7, 8"
 */
+// Создаю переменную num для N с запросом на ввод
+Console.Clear();
+int num = StringToInt("Пожалуйста, введите натуральное число больше 1: ");
+VyvodOtOdnogoDoN((uint)num);
+
+uint VyvodOtOdnogoDoN(uint n)
+{
+    if (n == 0)
+        return 1;
+    Console.Write($"{n} ");
+    VyvodOtOdnogoDoN(n - 1);
+    return 1;
+}
+
+int StringToInt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
