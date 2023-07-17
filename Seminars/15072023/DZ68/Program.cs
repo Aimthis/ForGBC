a32d7811f5426dccccb82637d5c7bf09d64cf194
aimthis <aimthis@mail.ru>
2023-07-17 21:30:09 +0500

    point 21:30 DZ68 seminar 15072023

--- Seminars/15072023/DZ68/Program.cs
+++ Seminars/15072023/DZ68/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+// Создаю переменную firstNum для M с запросом на ввод
+Console.Clear();
+int firstNum = StringToInt("Пожалуйста, введите первое натуральное число M для функции Аккермана: ");
+
+// Создаю переменную secondNum для N с запросом на ввод
+Console.Clear();
+int secondNum = StringToInt("Пожалуйста, введите второе натуральное число N для функции Аккермана: ");
+
+
+
+
+
+int StringToInt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
