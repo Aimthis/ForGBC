d5f79ed6e7b536510e70880d6e62320b1f294ad0
aimthis <aimthis@mail.ru>
2023-07-27 11:13:58 +0500

    start Итоговая контрольная работа по основному блоку 27072023

--- Longestsubstring/Program.cs
+++ Longestsubstring/Program.cs
@@ -1,6 +1,6 @@
 ﻿public class Solution {
     public int LengthOfLongestSubstring(string s) {
-                                                                 
+        string substring = string.Empty;                                                         
         string LongestSubstring = string.Empty;
         for(int end = 0; end < s.Length; end ++){
             for(int i = 0; i < s.Length; i++){
@@ -8,7 +8,7 @@
                     substring += s[substring.Length];
             }
             if(substring.Length < s.Length){
-                string check = substring[substring.Length - 1];
+                string check = substring.Substring(0,substring.Length - 1);
                 for(int j = substring.Length + 1; j < s.Length; j++){
                     if(check[j - 1] != s[j])
                         check += s[j];
@@ -19,5 +19,6 @@
                 LongestSubstring = check;
             }
         }        
+        return LongestSubstring.Length;
     }
 }
