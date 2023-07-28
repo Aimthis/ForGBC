d8d0f9d65ebddb345289de0f5476d5ed906c96a6
aimthis <aimthis@mail.ru>
2023-07-29 03:00:00 +0500

    point 2:59 edit Longestsubstring/Program.cs 28072023

--- Longestsubstring/Program.cs
+++ Longestsubstring/Program.cs
@@ -5,14 +5,14 @@
         string substring = s[0].ToString();
         string check = string.Empty;
         string LongestSubstring = string.Empty;
-        for (int i = 0; substring != s[0..substring.Length].ToString(); i++)
+        for (int i = 0; s.Contains(substring) == false; i++)
         {
             if (substring[i] != s[i + 1])
                 substring += s[i + 1];
             else
             {
                 check = s[i + 1].ToString();
-                for (int j = i + 1; check != s[i..check.Length].ToString(); j++)
+                for (int j = i + 1; s.Contains(check) == false; j++)
                 {
                     if (check[j] != s[j + 1])
                         check += s[j + 1];
@@ -20,9 +20,9 @@
                     break;
             }
         }
-        if (check.Length > substring.Length && check.Length + substring.Length > s.Length)
+        if (check.Length > substring.Length)
             LongestSubstring = check;
-        if (check.Length < substring.Length && check.Length + substring.Length > s.Length)
+        if (check.Length < substring.Length)
             LongestSubstring = substring;
         return LongestSubstring.Length;
     }
