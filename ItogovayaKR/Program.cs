59884f0a9e49e730c6eb3aacb32f22454fb760f1
aimthis <aimthis@mail.ru>
2023-07-28 11:11:26 +0500

    edit add input by while c# Program.cs 28072023

--- ItogovayaKR/Program.cs
+++ ItogovayaKR/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+ Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+ При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/ 
+Console.Clear();
+Console.WriteLine("Хотите начать ввод данных? Нажмите - Y (да) чтобы начать. Нажмите - N (нет) чтобы завершить");
+string inputString = string.Empty;
+string value = Console.ReadLine();
+while (true)
+{
+    if(value == "n" || value == "N")
+        break;
+    if(value == "y" || value == "Y"){
+    Console.WriteLine();
+        Console.Write("Введите данные : ");
+        value = Console.ReadLine();
+        inputString += value + "<-*->";
+        Console.Clear();
+        Console.WriteLine("Желаете ли добавить другие данные? Нажмите - Y (да) чтобы начать. Нажмите - N (нет) чтобы завершить");
+    }
+    else{
+        Console.WriteLine("Пожалуйста нажмите Y или N");
+        value = Console.ReadLine();
+    }
+}
+Console.WriteLine(inputString);
